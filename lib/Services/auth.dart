5a18443484b010e8583b1c8c47003c8d66b3a22a
Rishu <rishush804@gmail.com>
2023-04-17 22:18:03 +0530

    signin taken

--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -1,7 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:podcast/Modal/UserModel.dart';
 
 class AuthService{
   final FirebaseAuth _auth = FirebaseAuth.instance ;
 
+  UserModel? _userFromFirebaseUser(User? user){
+    return (user!=null)? UserModel(uid: user.uid): null;
+  }
 
+  Stream<UserModel?> get user{
+    return _auth.authStateChanges()
+        .map(_userFromFirebaseUser);
+  }
 }
