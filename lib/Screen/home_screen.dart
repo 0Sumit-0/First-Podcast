fb3f5e8b707b1114300e386b6abc79f1f6ca536e
Rishu <rishush804@gmail.com>
2023-04-17 18:36:57 +0530

    updated dependencies

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -25,8 +25,8 @@ class _HomeScreenState extends State<HomeScreen> {
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 5),
             child: SizedBox(
               height:50,
               child: Center(
@@ -43,7 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           SizedBox(height: 15),
-          Padding(
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 16),
             child: Text(
               'Categories',
--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -3,7 +3,5 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService{
   final FirebaseAuth _auth = FirebaseAuth.instance ;
 
-  Future signInAnon() async{
 
-  }
 }
