2109eddf8ae7b7ba405ecb0eae7ac6e73267d6e1
Rishu <rishush804@gmail.com>
2023-04-18 22:06:13 +0530

    emailID and Passwd in auth

--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -12,4 +12,24 @@ class AuthService{
     return _auth.authStateChanges()
         .map(_userFromFirebaseUser);
   }
+
+  Future registerWithEmailAndPasswd(String email, String passwd) async {
+    try{
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: passwd) ;
+      User? myuser = result.user;
+      return _userFromFirebaseUser(myuser);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async{
+    try {
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
 }
